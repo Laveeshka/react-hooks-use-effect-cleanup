138b801ae79d7a9e4748005b1829c3b5a82003b7
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-18 09:24:18 +1100

    do code along

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,14 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    //returning a cleanup function with the first arg, the callback function, of useEffect
+    return function cleanup() {
+      clearInterval(timerID);
+    }
   }, []);
 
   return <div>{time.toString()}</div>;
